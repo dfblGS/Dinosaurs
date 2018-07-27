6494d8d60bf45f9706e5144250d822ac2dc506ff
Brandon-Lao <brandon.lao11@gmail.com>
2018-07-27 12:32:34 -0400

    Redirect working

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -1,32 +1,42 @@
-import React, {Component} from 'react';
-import {CardElement, injectStripe} from 'react-stripe-elements';
+import React, {Component} from 'react'
+import {CardElement, injectStripe} from 'react-stripe-elements'
 import axios from 'axios'
+import { Redirect } from 'react-router'
 
 class CheckoutForm extends Component {
   constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
+    super(props)
+    this.submit = this.submit.bind(this)
+    this.state = {
+      setRedirect: false
+    }
   }
 
   async submit(event) {
-  	event.preventDefault()
-  	//Invalid API key, make sure they're working
-  	let {token} = await this.props.stripe.createToken();
-  	token.price = 3000;
-  	let response = await axios.post("/api/charge", token);
-    console.log(response)
-    // User clicked submit
+    try {
+      event.preventDefault()
+      let {token} = await this.props.stripe.createToken()
+      token.price = 3000
+      let response = await axios.post('/api/charge', token)
+      console.log(response)
+      if (response.status === 200) {
+        this.setState({setRedirect: true})
+      }
+    } catch (error) {
+      console.log('Error: ',error)
+    }
   }
 
   render() {
+    if (this.state.setRedirect) {return (<Redirect to={'/'} />)}
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
         <CardElement />
         <button onClick={this.submit}>Send</button>
       </div>
-    );
+    )
   }
 }
 
-export default injectStripe(CheckoutForm);
+export default injectStripe(CheckoutForm)
